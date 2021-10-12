159c6c0c370e4f0d6208496d73ba387ce3ce0d44
Alejandro <alejandro.mfe@gmail.com>
2021-10-12 12:11:10 -0300

    add edit action

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -25,33 +25,50 @@ namespace eTickets.Controllers
             return View(data);
         }
 
+        //Get: Actors/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails is null) return View("NotFound");
+            return View(actorDetails);
+        }
+        
         //Get: Actors/Create
         public IActionResult Create()
         {
             return View();
         }
+        
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName,Bio,ProfilePictureURL")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+                return View(actor);
+            /** 
+             * Si hay errores en la validación de datos
+             * retorna la misma vista con los mensajes de error
+             * del ModelState
+             */
 
-        //Get: Actors/Details/1
-        public async Task<IActionResult> Details(int id)
+            await _service.AddAsync(actor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Actors/Edit
+        public async Task<IActionResult> EditAsync(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
-            
-            if (actorDetails is null) return View("Empty");
+            if (actorDetails is null) return View("NotFoud");
             return View(actorDetails);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("FullName,Bio,ProfilePictureURL")]Actor actor)
+        public async Task<IActionResult> Edit(int id,[Bind("Id,FullName,Bio,ProfilePictureURL")] Actor actor)
         {
-            if (!ModelState.IsValid)
-                return View(actor); 
-                /** 
-                 * Si hay errores en la validación de datos
-                 * retorna la misma vista con los mensajes de error
-                 * del ModelState
-                 */
+            if (!ModelState.IsValid) return View(actor);
 
-            await _service.AddAsync(actor);
+            await _service.UpdateAsync(id, actor);
             return RedirectToAction(nameof(Index));
         }
     }
